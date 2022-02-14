695b657ed1bcff827520d3a7c1b9953f5ca50881
Rodion Ryzhov <65biso2@gmail.com>
2022-02-14 15:49:33 +0700

    code changes

--- Animation/ArrayMovementAnimator.cs
+++ Animation/ArrayMovementAnimator.cs
@@ -4,14 +4,15 @@ using System.Text;
 using System.Windows.Media.Imaging;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 
-namespace WpfApp3.Animation
+namespace WpfApp3
 {
 	class GraphicalObject
 	{
 		public Bitmap Image;
 		public Point LeftUpperCornerLocation;
-		public Color CurrentColor;
+		public Color CurrentColor => Image.GetPixel(0, 0);
 		public int LayerId;
 		private Graphics ImageGraphics;
 		private Rectangle FullSizeRect;
@@ -29,15 +30,23 @@ namespace WpfApp3.Animation
 	}
 	class ArrayMovementAnimator
 	{
-		public Bitmap CurrentStateFrame;
-		public int FrameWidth, FrameHeight;
-		public int[] AnimatedArray;
-		public SolidBrush CommonElementBrush = new SolidBrush(System.Drawing.Color.LightBlue);
-		public SolidBrush SelectedElementBrush = new SolidBrush(System.Drawing.Color.DarkGreen);
-		public SolidBrush BackgroundBrush = new SolidBrush(System.Drawing.Color.LightGray);
+		const int DefaultNumOfFrames = 60;
+		const int SpaceBetweenColumns = 2;
+
+		public event EventHandler FrameUpdated;
+
+
+		public Bitmap CurrentFrame;
+		public BitmapImage CurrentFrameImage => BitmapToImageSource(CurrentFrame);
+		public int FrameWidth { get; private set; }
+		public int FrameHeight { get; private set; }
+		public int[] AnimatedArray { get; private set; }
+		public SolidBrush CommonElementBrush { get; set; } = new SolidBrush(System.Drawing.Color.LightBlue);
+		public SolidBrush SelectedElementBrush { get; set; } = new SolidBrush(System.Drawing.Color.DarkGreen);
+		public SolidBrush BackgroundBrush { get; set; } = new SolidBrush(System.Drawing.Color.WhiteSmoke);
 		private GraphicalObject[] ArrayElementsAsGraphicalObjects;
-		public int ElementWidth;
-		public int SpaceBetweenColumns = 2;
+		private int ElementWidth => FrameWidth / AnimatedArray.Length - ((AnimatedArray.Length - 1) * SpaceBetweenColumns);
+
 
 		public ArrayMovementAnimator(int FrameWidth, int FrameHeight, int[] AnimatedArray)
 		{
@@ -46,7 +55,9 @@ namespace WpfApp3.Animation
 			this.AnimatedArray = AnimatedArray;
 			// (FrameWidth-(AnimatedArray.Length-1)*SpaceBetweenColumns) / AnimatedArray.Length  -- оставить место в размере 2 пикселей для пробелов между столбиками
 			this.ArrayElementsAsGraphicalObjects = ArrayToGraphicalObjects(this.AnimatedArray, (FrameWidth - (AnimatedArray.Length - 1) * SpaceBetweenColumns) / AnimatedArray.Length, FrameHeight);
-			ElementWidth = ArrayElementsAsGraphicalObjects[0].Image.Width;
+			this.CurrentFrame = new Bitmap(FrameWidth, FrameHeight);
+			this.RenderCurrentFrame();
+			//this.CurrentFrameImage = BitmapToImageSource(CurrentFrame);
 		}
 
 		private GraphicalObject[] ArrayToGraphicalObjects(int[] Arr, int OneElementWidth, int MaxHeight)
@@ -58,8 +69,9 @@ namespace WpfApp3.Animation
 			{
 				var CurrElHeight = Arr[i] * HeightCoeff;
 				Out[i] = new GraphicalObject(
-					 new Point(this.ElementWidth * i + this.SpaceBetweenColumns * i, MaxHeight - CurrElHeight),
+					 new Point(this.ElementWidth * i + SpaceBetweenColumns * i, MaxHeight - CurrElHeight),
 					 new Size(ElementWidth, CurrElHeight));
+				Out[i].SetColor(this.CommonElementBrush.Color);
 			}
 
 			return Out;
@@ -71,60 +83,73 @@ namespace WpfApp3.Animation
 			{
 				for (int SourceY = 0; SourceY < SourceBitmap.Height && (SourceY + DestinationLeftUpperCorner.Y) < DestinationBitmap.Height; SourceY++)
 				{
-					DestinationBitmap.SetPixel(SourceX + DestinationLeftUpperCorner.X, SourceY + DestinationLeftUpperCorner.Y, SourceBitmap.GetPixel(SourceY, SourceY));
+					var SetX = SourceX + DestinationLeftUpperCorner.X;
+					var SetY = SourceY + DestinationLeftUpperCorner.Y;
+					var SourcePixel = SourceBitmap.GetPixel(SourceX, SourceY);
+					DestinationBitmap.SetPixel(SetX, SetY, SourcePixel);
 				}
 			}
 		}
 
-		public Bitmap RenderCurrentFrame()
+		public void RenderCurrentFrame()
 		{
-			Bitmap Out = new Bitmap(this.FrameWidth, this.FrameHeight);
-			var Gr = Graphics.FromImage(Out);
+			var Gr = Graphics.FromImage(this.CurrentFrame);
 
 			var GOs = this.ArrayElementsAsGraphicalObjects;
 
-			Gr.FillRectangle(this.BackgroundBrush, 0, 0, Out.Width, Out.Height);
+			Gr.FillRectangle(this.BackgroundBrush, 0, 0, this.CurrentFrame.Width, this.CurrentFrame.Height);
 
 			var id = 0;
 			foreach (var GO in this.ArrayElementsAsGraphicalObjects.OrderBy(x => x.LayerId))
-				CopyBitmapToAnother(GO.Image, Out, CalculateLeftUpperCornerLocationOfTheElement(id++));
-
-			return Out;
+				CopyBitmapToAnother(GO.Image, this.CurrentFrame, CalculateLeftUpperCornerLocationOfTheElement(id++));
 		}
 
 		private Point CalculateLeftUpperCornerLocationOfTheElement(int ElementId)
-			=> new Point(this.ElementWidth * ElementId + this.SpaceBetweenColumns * ElementId,this.FrameHeight- this.ArrayElementsAsGraphicalObjects[ElementId].Image.Height);
+			=> new Point(this.ElementWidth * ElementId + SpaceBetweenColumns * ElementId, this.FrameHeight - this.ArrayElementsAsGraphicalObjects[ElementId].Image.Height);
 
 		public IEnumerator<Action> FillAnimationActions(GraphicalObject ArrayElement, Color TargetColor, int FramesNum)
 		{
-			double RedStep = (TargetColor.R - ArrayElement.CurrentColor.R) / FramesNum;
-			double GreenStep = (TargetColor.G - ArrayElement.CurrentColor.G) / FramesNum;
-			double BlueStep = (TargetColor.B - ArrayElement.CurrentColor.B) / FramesNum;
+			double RedStep = (TargetColor.R - ArrayElement.CurrentColor.R) / (double)(FramesNum);
+			double GreenStep = (TargetColor.G - ArrayElement.CurrentColor.G) / (double)(FramesNum);
+			double BlueStep = (TargetColor.B - ArrayElement.CurrentColor.B) / (double)(FramesNum);
 
-			for (int i = 0; i < FramesNum; i++)
+			for (int i = 0; i < FramesNum-1; i++)
 			{
-				int CurrentRed = (int)(RedStep * i + ArrayElement.CurrentColor.R);
-				int CurrentGreen = (int)(GreenStep * i + ArrayElement.CurrentColor.G);
-				int CurrentBlue = (int)(BlueStep * i + ArrayElement.CurrentColor.B);
+				int CurrentRed = (int)(RedStep + ArrayElement.CurrentColor.R);
+				int CurrentGreen = (int)(GreenStep + ArrayElement.CurrentColor.G);
+				int CurrentBlue = (int)(BlueStep + ArrayElement.CurrentColor.B);
 
-				yield return new Action(() => ArrayElement.SetColor(Color.FromArgb(CurrentRed, CurrentGreen, CurrentBlue)));
+				if (CurrentRed < 0) CurrentRed = 0;
+				if (CurrentGreen < 0) CurrentGreen = 0;
+				if (CurrentBlue < 0) CurrentBlue = 0;
+
+				yield return new Action(() =>
+				{
+					ArrayElement.SetColor(Color.FromArgb(CurrentRed, CurrentGreen, CurrentBlue));
+					FrameUpdated.Invoke(this, EventArgs.Empty);
+				});
 			}
+			yield return new Action(() =>
+			{
+				ArrayElement.SetColor(TargetColor);
+				FrameUpdated.Invoke(this, EventArgs.Empty);
+			});
 		}
 		/// <summary>
 		/// Возвращает делегаты Action, которые должны быть выполнены для выделения элемента.
 		/// </summary>
-		public IEnumerator<Action> ToSelectedColorActions(int ElementId, int FramesNum = 60)
+		public IEnumerator<Action> ToSelectedColorActions(int ElementId, int FramesNum = DefaultNumOfFrames)
 			=> FillAnimationActions(ArrayElementsAsGraphicalObjects[ElementId], SelectedElementBrush.Color, FramesNum);
 		/// <summary>
 		/// Возвращает делегаты Action, которые должны быть выполнены для снятия выделения элемента.
 		/// </summary>
-		public IEnumerator<Action> ToCommonColorActions(int ElementId, int FramesNum = 60)
+		public IEnumerator<Action> ToCommonColorActions(int ElementId, int FramesNum = DefaultNumOfFrames)
 			=> FillAnimationActions(ArrayElementsAsGraphicalObjects[ElementId], CommonElementBrush.Color, FramesNum);
 
 		/// <summary>
 		/// Возвращает делегаты Action, которые должны быть выполнены для перемещения объекта.
 		/// </summary>
-		public IEnumerator<Action> MoveAnimationActions(int ElementId, Point DestionationPoint, int FramesNum = 60)
+		public IEnumerator<Action> MoveAnimationActions(int ElementId, Point DestionationPoint, int FramesNum = DefaultNumOfFrames)
 		{
 			GraphicalObject GO = this.ArrayElementsAsGraphicalObjects[ElementId];
 
@@ -137,6 +162,7 @@ namespace WpfApp3.Animation
 				{
 					GO.LeftUpperCornerLocation.X += (int)(StepX * i);
 					GO.LeftUpperCornerLocation.Y += (int)(StepY * i);
+					FrameUpdated.Invoke(this, EventArgs.Empty);
 				});
 			}
 		}
@@ -148,10 +174,78 @@ namespace WpfApp3.Animation
 		/// <param name="DestionationPoint"></param>
 		/// <param name="FramesNum"></param>
 		/// <returns></returns>
-		public IEnumerator<Action[]> SwapAnimationActions(int ElementId, Point DestionationPoint, int FramesNum = 60)
+		public IEnumerator<Action[]> SwapAnimationActions(int FirstElementId, int SecondElementId, int FramesNum = DefaultNumOfFrames)
 		{
-			
+			var UpdateInvoker = new Action(() => FrameUpdated.Invoke(this, EventArgs.Empty));
+
+			var FirstGO = this.ArrayElementsAsGraphicalObjects[FirstElementId];
+			var SecondGO = this.ArrayElementsAsGraphicalObjects[SecondElementId];
+
+			FirstGO.LayerId = 1;
+			SecondGO.LayerId = 1;
+
+			var FirstSelectActions = ToSelectedColorActions(FirstElementId);
+			var SecondSelectActions = ToSelectedColorActions(SecondElementId);
+
+			while (FirstSelectActions.MoveNext() && SecondSelectActions.MoveNext())
+			{
+				var CurrentFirst = FirstSelectActions.Current;
+				var CurrentSecond = SecondSelectActions.Current;
+				yield return new Action[] { CurrentFirst, CurrentSecond, UpdateInvoker };
+			}
+
+			var FirstElementDestination = new Point(SecondGO.LeftUpperCornerLocation.X, FirstGO.LeftUpperCornerLocation.Y);
+			var SecondElementDestination = new Point(FirstGO.LeftUpperCornerLocation.X, SecondGO.LeftUpperCornerLocation.Y);
+
+			var FirstMoveActions = MoveAnimationActions(FirstElementId, FirstElementDestination);
+			var SecondMoveActions = MoveAnimationActions(SecondElementId, SecondElementDestination);
+
+			while (FirstMoveActions.MoveNext() && SecondMoveActions.MoveNext())
+			{
+				var CurrentFirst = FirstMoveActions.Current;
+				var CurrentSecond = SecondMoveActions.Current;
+				yield return new Action[] { CurrentFirst, CurrentSecond, UpdateInvoker };
+			}
+
+			var FirstUnselectActions = ToCommonColorActions(FirstElementId);
+			var SecondUnselectActions = ToCommonColorActions(SecondElementId);
+
+			while (FirstUnselectActions.MoveNext() && SecondUnselectActions.MoveNext())
+			{
+				var CurrentFirst = FirstUnselectActions.Current;
+				var CurrentSecond = SecondUnselectActions.Current;
+				yield return new Action[] { CurrentFirst, CurrentSecond, UpdateInvoker };
+			}
+
+			FirstGO.LayerId = 0;
+			SecondGO.LayerId = 0;
+
+			(FirstGO, SecondGO) = (SecondGO, FirstGO);
 		}
 
+		public void PerformSwapInSourceArrays(int i1, int i2)
+		{
+			(AnimatedArray[i1], AnimatedArray[i2]) = (AnimatedArray[i2], AnimatedArray[i1]);
+		}
+
+
+
+
+		// https://stackoverflow.com/a/22501616/11325184
+		private BitmapImage BitmapToImageSource(Bitmap bitmap)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+				memory.Position = 0;
+				BitmapImage bitmapimage = new BitmapImage();
+				bitmapimage.BeginInit();
+				bitmapimage.StreamSource = memory;
+				bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapimage.EndInit();
+
+				return bitmapimage;
+			}
+		}
 	}
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WpfApp3
 {
@@ -20,9 +21,58 @@ namespace WpfApp3
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		ArrayMovementAnimator MyAnim;
 		public MainWindow()
 		{
 			InitializeComponent();
+			MyAnim = new ArrayMovementAnimator(400, 300, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+			ArrayIMAGE.Source = MyAnim.CurrentFrameImage;
+			MyAnim.FrameUpdated += OnFrameUpdate;
+		}
+		private async void but1_Click(object sender, RoutedEventArgs e)
+		{
+			await Task.Run(() =>
+			{
+				var Acts = MyAnim.ToSelectedColorActions(1,5);
+				Acts.MoveNext();
+
+
+				while (Acts.MoveNext())
+				{
+					Acts.Current.Invoke();
+				}
+
+
+				Acts = MyAnim.ToSelectedColorActions(2, 5);
+				Acts.MoveNext();
+
+				while (Acts.MoveNext())
+				{
+					Acts.Current.Invoke();
+				}
+
+				Acts = MyAnim.ToCommonColorActions(1, 5);
+				Acts.MoveNext();
+
+				while (Acts.MoveNext())
+				{
+					Acts.Current.Invoke();
+				}
+				Acts = MyAnim.ToCommonColorActions(2, 5);
+				Acts.MoveNext();
+
+				while (Acts.MoveNext())
+				{
+					Acts.Current.Invoke();
+				}
+			});
+		}
+
+		private void OnFrameUpdate(object sender, EventArgs e)
+		{
+			MyAnim.RenderCurrentFrame();
+			MyAnim.CurrentFrameImage.Freeze();
+			Dispatcher.Invoke(new Action(()=> ArrayIMAGE.Source = MyAnim.CurrentFrameImage));
 		}
 	}
 }
